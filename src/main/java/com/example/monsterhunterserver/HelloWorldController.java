bcbd1676c3999a648339f6df37655b368765182c
MarynaKor <Maryna.Kor@gmail.com>
2022-12-10 15:59:21 +0100

    first commit

--- src/main/java/com/example/monsterhunterserver/HelloWorldController.java
+++ src/main/java/com/example/monsterhunterserver/HelloWorldController.java
@@ -1,2 +1,18 @@
-package com.example.monsterhunterserver;public class HelloWorldController {
+package com.example.monsterhunterserver;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String index(){
+        return "Greeting!";
+    }
+
+    @GetMapping ("/hello")
+    public String hello() {
+        return "Hello World";
+    }
 }
